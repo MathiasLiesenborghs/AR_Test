4fc874e3163fae5f82ed5254cdeee0637a6812c8
Mathias <liesenborghsmemploi@gmail.com>
2024-11-08 12:15:41 +0100

    Random changes

--- Assets/Scripts/MathiasARShoot.cs
+++ Assets/Scripts/MathiasARShoot.cs
@@ -7,34 +7,34 @@ public class MathiasARShoot : MonoBehaviour
     public GameObject particles;
     void Update()
     {
-        // Gestion des entrées tactiles
+        
         for (int i = 0; i < Input.touchCount; ++i)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
 
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit)) // Utilise RaycastHit pour obtenir l'impact exact
+            if (Physics.Raycast(ray, out hit)) 
             {
-                // Instancier les particules à la position de l'impact du rayon
+                
                 GameObject particlesInstance = Instantiate(particles, hit.point, Quaternion.identity);
 
-                // Détruire les particules après 1 seconde
+                
                 Destroy(particlesInstance, 1f);
             }
         }
 
-        // Gestion des clics de souris
-        if (Input.GetMouseButtonDown(0)) // 0 est pour le clic gauche de la souris
+        
+        if (Input.GetMouseButtonDown(0)) 
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit)) // Utilise RaycastHit pour obtenir l'impact exact
+            if (Physics.Raycast(ray, out hit)) 
             {
-                // Instancier les particules à la position de l'impact du rayon
+                
                 GameObject particlesInstance = Instantiate(particles, hit.point, Quaternion.identity);
 
-                // Détruire les particules après 1 seconde
+                
                 Destroy(particlesInstance, 1f);
             }
         }
