89609b977f8b3b071ad59149ee303b8695ea39a8
Mathias <liesenborghsmemploi@gmail.com>
2024-11-12 10:27:51 +0100

    Set up for sound effect

---
+++ Assets/Scripts/MathiasSound.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MathiasSound : MonoBehaviour
+{
+    public AudioClip SoundSpawn;
+    public AudioClip SoundDestroy;
+    private AudioSource AudioSource;
+    void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+
+        if (SoundSpawn != null)
+        {
+            AudioSource.PlayOneShot(SoundSpawn);
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (SoundDestroy != null)
+        {
+            AudioSource.DestroyImmediate(SoundDestroy);
+        }
+    }
+}
